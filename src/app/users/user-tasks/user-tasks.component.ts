3ab9876d8c792152f2044c9318e53ef037dc38e5
codetivated <melisa@heying.us>
2025-10-21 14:08:27 -0500

    extract dynamic route params via Inputs

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
+import { User } from '../user/user.model';
 
 @Component({
   selector: 'app-user-tasks',
@@ -6,4 +8,12 @@ import { Component } from '@angular/core';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent {
+  userId = input.required<string>();
+  private usersService = inject(UsersService);
+
+  userName = computed(() => {
+    return this.usersService.users.find((user) => user.id === this.userId())
+      ?.name;
+  });
+}
