1e8a819c9831209006cfb891bec55e8473279213
codetivated <melisa@heying.us>
2025-10-21 14:12:40 -0500

    add note for using @Input() instead of input()

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -17,3 +17,19 @@ export class UserTasksComponent {
       ?.name;
   });
 }
+
+// Extracting Dynamic Route Parameters via @Input()
+
+// You don't have to extract dynamic route parameters via signal-based inputs - you can also use @Input()-based inputs!
+
+// To do this, you can define the input like this:
+
+// @Input({required: true}) userId!: string;
+// You can then use this input in any way needed - for example inside of a getter function.
+
+// If you want to run some code whenever the input value changes, you can also define an extra setter function that contains the to-be-executed code. Like this:
+
+// @Input()
+// set userId(uid: string) {
+//   console.log(uid);
+// }
