578dc91ce41a9ac5365372a5747359b3817d7aea
codetivated <melisa@heying.us>
2025-11-21 13:41:29 -0600

    split route definition for user routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Route } from '@angular/router';
-import { TasksComponent } from './tasks/tasks.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { routes as userRoutes } from './users/users.routes';
 
 export const routes: Route[] = [
   {
@@ -13,21 +12,7 @@ export const routes: Route[] = [
   {
     path: 'users/:userId',
     component: UserTasksComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'tasks',
-        pathMatch: 'prefix',
-      },
-      {
-        path: 'tasks',
-        component: TasksComponent,
-      },
-      {
-        path: 'tasks/new',
-        component: NewTaskComponent,
-      },
-    ],
+    children: userRoutes,
   },
   { path: '**', component: NotFoundComponent },
 ];
