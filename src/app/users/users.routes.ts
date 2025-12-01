e6992194b1dcb05ee84221b1db03d96375408551
codetivated <melisa@heying.us>
2025-12-01 14:55:15 -0600

    use runGuardsAndResolvers with paramsOrQueryParamsChange to ensure that the resolver reruns during a param or query change

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,19 +1,26 @@
 import { Routes } from '@angular/router';
-import { TasksComponent } from '../tasks/tasks.component';
+
+import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
 import { NewTaskComponent } from '../tasks/new-task/new-task.component';
 
 export const routes: Routes = [
   {
     path: '',
     redirectTo: 'tasks',
-    pathMatch: 'prefix',
+    pathMatch: 'full',
   },
   {
-    path: 'tasks',
+    path: 'tasks', // <your-domain>/users/<uid>/tasks
     component: TasksComponent,
+    runGuardsAndResolvers: 'paramsOrQueryParamsChange',
+    resolve: {
+      userTasks: resolveUserTasks,
+    },
   },
   {
     path: 'tasks/new',
     component: NewTaskComponent,
   },
 ];
+
+// using runGuardsAndResolvers: 'paramsOrQueryParamsChange' to ensure that the resolver re-runs when either route parameters or query parameters change
