6d81a1ed9e22914b38da5772aa643f0b51f6a02a
codetivated <melisa@heying.us>
2025-12-01 15:24:44 -0600

    add Route Guard example (get redirected depending on guard logic)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Route } from '@angular/router';
+import { CanMatchFn, RedirectCommand, Route, Router } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import {
   resolvePageTitle,
@@ -7,6 +7,19 @@ import {
 } from './users/user-tasks/user-tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { routes as userRoutes } from './users/users.routes';
+import { inject } from '@angular/core';
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+  const router = inject(Router);
+  const shouldGetAccess = Math.random(); // Replace with real logic
+  console.log('Should get access: ' + shouldGetAccess);
+
+  if (shouldGetAccess > 0.5) {
+    return true;
+  } else {
+    return new RedirectCommand(router.parseUrl('/unauthorized'));
+  }
+};
 
 export const routes: Route[] = [
   {
@@ -18,6 +31,7 @@ export const routes: Route[] = [
     path: 'users/:userId',
     component: UserTasksComponent,
     children: userRoutes,
+    canMatch: [dummyCanMatch],
     data: {
       message: 'Hello!',
     },
@@ -28,3 +42,5 @@ export const routes: Route[] = [
   },
   { path: '**', component: NotFoundComponent },
 ];
+
+// here we define a dummyCanMatch function that randomly allows or denies access to the /users/:userId route. If access is denied, it redirects to an /unauthorized page. This is just for demonstration for using canMatch; in a real application, you would replace the random logic with actual authentication or authorization checks.
